5f7a7e3deba4ec3b8436c345d8ef87154e77938c
eascarrunz <eascarrunz.dev@mailfence.com>
2022-03-21 07:29:42 -0300

    add Newick parser

--- src/RootedTrees.jl
+++ src/RootedTrees.jl
@@ -33,7 +33,34 @@ export link!, cut!, swap!
 include("tree_builders.jl")
 export symmetric_tree
 
-include("newick.jl")
+include("read_newick.jl")
+export parsenewick
+
+foo = IOBuffer("((c1,c2,c3)x,s)p;")
+tree = parsenewick(foo)
+
+test_strings = [
+    "(,,(,));",                              ## no nodes are named
+    "(A,B,(C,D));",                          ## leaf nodes are named
+    "(A,B,(C,D)E)F;",                        ## all nodes are named
+    "(:0.1,:0.2,(:0.3,:0.4):0.5);",         ## all but root node have a distance to parent
+    "(:0.1,:0.2,(:0.3,:0.4):0.5):0.0;" ,     ## all have a distance to parent
+    "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);",      ## distances and leaf names (popular)
+    "(A:0.1, 
+    B:0.2 , (C:0.3,D:0.4) : 0.5 ) ;",      ## distances and leaf names (popular)
+    "(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F;",    ## distances and all names
+    "((B:0.2,(C:0.3,D:0.4)E:0.5)F:0.1)A;"   ## a tree rooted on a leaf node (rare)
+    ]
+
+parsenewick(test_strings[1])
+
+include("write_newick.jl")
 export newick, print_newick
+
+for str in test_strings
+    tree = parsenewick(str)
+    newick(tree)
+end
+
 
 end # module
---
+++ src/read_newick.jl
@@ -0,0 +1,159 @@
+const NEWICK_DELIM = ('(', ')', ',', ':', ';')
+const COMMENT_DELIM = ('[', ']')
+const ALL_DELIMS = union(NEWICK_DELIM, COMMENT_DELIM)
+
+"""
+Count the number of nodes in a Newick stream
+"""
+function _countnodes(io)
+
+    #TODO: Use this function for pre-allocating the node vector of the tree
+    #? Could use a `getfreenode` function that returns an unlinked node in the node vector
+    #? or a new node created with `addnode!`
+
+    #* The logic below could be simplified using `skipchars`
+
+    i = 0
+    chr = peek(io, Char)
+    while chr ≠ ';'
+        i += ((chr == '(') | (chr == ',')) ? 1 : 0
+        chr = read(io, Char)
+    end
+
+    return i
+end
+
+
+"""
+Read IO until any character in a collection of delimiters is found.
+"""
+function _readuntilany(io, delimset)
+
+    #TODO: Re-implement this method using `skipchars`
+
+    s = ""
+    c = peek(io, Char)
+    while c ∉ delimset
+        s *= read(io, Char)
+        c = peek(io, Char)
+    end
+
+    return s
+end
+
+
+"""
+Check that `query` is the current character in a IOStram, and read it. Raise exception if 
+it isn't. 
+"""
+function _readconfirm(io, query)
+    chr = read(io, Char)
+    if chr != query
+        @error "expected \'$(query)\' but got \'$(chr)\'"
+    end
+
+    return nothing
+end
+
+
+function _parsetext(io)
+    s = peek(io, Char)
+    if s == '\"'
+        s = readuntil(io, '\"')
+    elseif s == '\''
+        s = readuntil(io, '\'')
+    else
+        s = _readuntilany(io, ALL_DELIMS)
+    end
+    
+    return s
+end
+
+function _parsebranch(io)
+    #TODO: Check for a comment here
+    _readconfirm(io, ':')
+    s = _readuntilany(io, ALL_DELIMS)
+
+    return isempty(s) ? nothing : parse(Float64, s)
+end
+
+function _parseouter(io::IO, tree)
+
+    #? All this could just be included in `_parsechild`
+
+    labelstr = _parsetext(io)
+    chr = peek(io)
+    #TODO: Check for a comment here
+    brlen = chr == ':' ? _parsebranch(io) : nothing
+    
+    c = addnode!(tree, 1)
+    setlabel!(c, labelstr)
+    brlength!(c, brlen)
+    
+    return c
+end
+
+function _parsechild(io, tree)
+    chr = peek(io, Char)
+    if chr == '('
+        c = _parseinner(io, tree)
+    else
+        c = _parseouter(io, tree)
+    end
+    #TODO: Check for a comment here
+    
+    return c
+end
+
+function _parseinner(io::IO, tree)
+    _readconfirm(io, '(')
+    _whiteslurp(io)
+
+    
+    p = addnode!(tree, 1)
+    
+    chr = peek(io, Char)   #? Not necessary
+    c = _parsechild(io, tree)
+    link!(p => c)
+    _whiteslurp(io)
+    chr = peek(io, Char)
+    
+    while chr == ','
+        _readconfirm(io, ',')    # Eat the comma
+        _whiteslurp(io)
+        c = _parsechild(io, tree)
+        link!(p => c)
+        _whiteslurp(io)
+        chr = peek(io, Char)
+    end
+    
+    _readconfirm(io, ')')
+    
+    labelstr = _parsetext(io)
+    chr = peek(io)
+    brlen = chr == ':' ? _parsebranch(io) : nothing
+    
+    setlabel!(p, labelstr)
+    brlength!(p, brlen)
+    
+    return p
+end
+
+_whiteslurp(io) = skipchars(isspace, io)
+
+function parsenewick(io::IO)
+    tree = createtree(0)
+
+    _whiteslurp(io)
+    tree.root = _parseinner(io, tree)
+    _whiteslurp(io)
+
+    #TODO: Check for a comment here
+    
+    _readconfirm(io, ';')
+
+    return tree
+end
+
+parsenewick(s::String) = parsenewick(IOBuffer(s))
+
