4617ee4c164442da11fb71d2f8bf21a3e4256150
eascarrunz <eascarrunz.dev@mailfence.com>
2022-03-23 21:50:51 -0300

    add Makie plotting recipe

--- src/RootedTrees.jl
+++ src/RootedTrees.jl
@@ -1,5 +1,7 @@
 module RootedTrees
 
+using Requires
+
 const IDInt = UInt16    # Maximum of 65535
 
 #======================#
@@ -40,5 +42,13 @@ export parsenewick
 include("write_newick.jl")
 export newick, print_newick
 
+function __init__()
+    @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" begin
+        using Makie
+
+        include("plot.jl")
+        export plot, plot!
+    end
+end
 
 end # module
